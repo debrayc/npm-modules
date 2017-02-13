d2c256047cae854006e435e7a682d44ed0e93fcf
Learner <learner@LGs-MacBook-Pro.local>
2017-02-13 15:41:17 -0800

    implemented add function and test for function

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'NPM Modules for JS' });
 });
 
 module.exports = router;
